3cb2c0edfd1741c744eb91a6d3676b4831c29cfb
HanyHagar <hanymagdyhagar@gmail.com>
2026-08-05 22:04:03 +0300

    feat(settings): Add settings cubit to splash

--- lib/features/splash/views/splash_view.dart
+++ lib/features/splash/views/splash_view.dart
@@ -1,9 +1,29 @@
+
 import '../../../const/aseets.dart';
 import '../widgets/splash_body.dart';
 import 'package:flutter/material.dart';
+import '../../../di/server_locator.dart';
+import '../../settings/presentation/manager/settings_cubit.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      if(getIt<SettingsCubit>().state.isFirstTime) {
+        // Navigate to onboarding screen
+      } else {
+        // Navigate to home screen
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +37,7 @@ class SplashView extends StatelessWidget {
             fit: BoxFit.fill,
           ),
         ),
-        child: Center(child: SplashBody(),),
+        child: Center(child: SplashBody()),
       ),
     );
   }
