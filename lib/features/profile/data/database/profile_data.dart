6b48f6c85ecee5907d1edbf43fda58a6fa0d7469
HanyHagar <hanymagdyhagar@gmail.com>
2026-08-06 22:26:40 +0300

    feat(profile): Add profile data file

---
+++ lib/features/profile/data/database/profile_data.dart
@@ -0,0 +1,26 @@
+import '../models/profile_model.dart';
+import '../../../../core/services/hive_service.dart';
+
+class ProfileDatabase {
+  final HiveService hiveService;
+
+  const ProfileDatabase(this.hiveService);
+
+  static const boxName = 'profile';
+
+  Future<void> init() async {
+    await hiveService.openBox<ProfileModel>(boxName);
+  }
+
+  Future<void> save(ProfileModel profile) async {
+    await hiveService.put(boxName: boxName, key: 'user', value: profile);
+  }
+
+  ProfileModel? get() {
+    return hiveService.get<ProfileModel>(boxName: boxName, key: 'user');
+  }
+
+  Future<void> delete() async {
+    await hiveService.delete(boxName: boxName, key: 'user');
+  }
+}
