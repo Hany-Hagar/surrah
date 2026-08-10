9bb00462b4761bed7184fe2a80b81a62c85fc97d
HanyHagar <hanymagdyhagar@gmail.com>
2026-08-10 21:09:43 +0300

    feat(profile): Update profile avatar

--- lib/core/widgets/profile_avatar.dart
+++ lib/core/widgets/profile_avatar.dart
@@ -1,8 +1,12 @@
 import 'dart:io';
+import '../utils/icon_broken.dart';
 import 'package:flutter/material.dart';
+import '../services/image_service.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../features/profile/presentation/manager/profile_cubit.dart';
-import '../utils/icon_broken.dart';
+import '../../features/profile/presentation/manager/profile_states.dart';
 
 class ProfileAvatar extends StatelessWidget {
   final double radius;
@@ -11,15 +15,19 @@ class ProfileAvatar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var image = ProfileCubit.get(context).profile.photo;
-    var isLocalPath =
-        image != null && image.isNotEmpty && !image.startsWith('http');
-    return Stack(
-      alignment: AlignmentDirectional.bottomEnd,
-      children: [
-        _Image(radius: radius, image: image, isLocalPath: isLocalPath),
-        if (showEditIcon) const _CameraIcon(),
-      ],
+    return BlocBuilder<ProfileCubit, ProfileStates>(
+      builder: (context, state) {
+        var image = ProfileCubit.get(context).profile.photo;
+        var isLocalPath =
+            image != null && image.isNotEmpty && !image.startsWith('http');
+        return Stack(
+          alignment: AlignmentDirectional.bottomEnd,
+          children: [
+            _Image(radius: radius, image: image, isLocalPath: isLocalPath),
+            if (showEditIcon) const _CameraIcon(),
+          ],
+        );
+      },
     );
   }
 }
@@ -58,17 +66,23 @@ class _CameraIcon extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
-    return Container(
-      padding: EdgeInsets.all(8.sp),
-      decoration: BoxDecoration(
-        color: theme.colorScheme.secondary,
-        shape: BoxShape.circle,
-        border: Border.all(color: theme.scaffoldBackgroundColor, width: 3),
-      ),
-      child: Icon(
-        IconBroken.Camera,
-        color: theme.scaffoldBackgroundColor,
-        size: 24.sp,
+    var cubit = ProfileCubit.get(context);
+    return GestureDetector(
+      onTap: () => ImageService.pickImage(
+        source: ImageSource.gallery,
+      ).then((value) => cubit.updateProfilePhoto(value?.path)),
+      child: Container(
+        padding: EdgeInsets.all(8.sp),
+        decoration: BoxDecoration(
+          color: theme.colorScheme.secondary,
+          shape: BoxShape.circle,
+          border: Border.all(color: theme.scaffoldBackgroundColor, width: 3),
+        ),
+        child: Icon(
+          IconBroken.Camera,
+          color: theme.scaffoldBackgroundColor,
+          size: 24.sp,
+        ),
       ),
     );
   }
